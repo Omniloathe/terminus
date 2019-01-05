8fd9a655ce4baf157f7d9146de2244885fa6b8d9
Soren Kierkegaard <manichaeus@protonmail.com>
2019-01-05 10:23:32 -0600

    Getting layout template set up

--- terminus/views.py
+++ terminus/views.py
@@ -1,7 +1,21 @@
 from terminus import app
+from flask import render_template
+
+testposts=['Iraeneus Argument','Modus Ponens','Monty Python Witch Proof']
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World'
+    return render_template('index.html',posts=testposts)
 
+@app.route('/about')
+def about():
+    return render_template('about.html',title='About Terminus')
+
+
+
+#probably doesn't work
+@app.route('/newarg')
+def new_arg():
+    return render_template('newargument.html')
