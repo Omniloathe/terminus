9223c4f902e3ac4ebebbe8c9f66d9b2804249d2f
Soren Kierkegaard <manichaeus@protonmail.com>
2019-01-05 04:17:54 -0600

    Just getting directory structure set up

---
+++ terminus/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app=Flask(__name__)
+app.config.from_object('config')
+
+# Now we can access the configuration variables via app.config["VAR_NAME"].
+
+import terminus.views
