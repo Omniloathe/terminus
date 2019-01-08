7909188c92601685d7d9d822043f9de1895a3f73
Soren Kierkegaard <manichaeus@protonmail.com>
2019-01-08 03:29:45 -0600

    Finished login view handler

--- terminus/views.py
+++ terminus/views.py
@@ -1,7 +1,11 @@
 from terminus import app
-from flask import render_template
+from flask import render_template, session, redirect, url_for
+from terminus.forms import LoginForm
 
 testposts=['Iraeneus Argument','Modus Ponens','Monty Python Witch Proof']
+
+def my_render():
+    pass
 
 
 @app.route('/')
@@ -13,9 +17,13 @@ def index():
 def about():
     return render_template('about.html',title='About Terminus')
 
-@app.route('/login',methods=['POST'])
+@app.route('/login',methods=['GET','POST'])
 def login():
-    pass
+    loginform=LoginForm()
+    if loginform.validate_on_submit():
+        session['username']=loginform.username.data
+        return redirect(url_for('index'))
+    return render_template('login.html',loginform=loginform)
 
 
 
@@ -32,8 +40,8 @@ def js_test():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html',e=e), 404
 
 @app.errorhandler(500)
 def internal_service_error(e):
-    return render_template('500.html'), 500
+    return render_template('500.html',e=e), 500
